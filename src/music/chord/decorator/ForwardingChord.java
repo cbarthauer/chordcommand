c0cd2fd5f047db9c3130cc9fafb5fbfa72c74c4d
cbarthauer <devnull@localhost>
2012-04-13 14:32:51 -0400

    Adjusted logging settings.

--- src/music/chord/decorator/ForwardingChord.java
+++ src/music/chord/decorator/ForwardingChord.java
@@ -17,8 +17,8 @@ public class ForwardingChord implements Chord {
 	
 	@Override
 	public NoteName noteNameFromChordMember(ChordMember chordMember) {
-		logger.debug("chord: " + chord);
-		logger.debug("chordMember: " + chordMember);
+		logger.trace("chord: " + chord);
+		logger.trace("chordMember: " + chordMember);
 		return chord.noteNameFromChordMember(chordMember);
 	}
 	
