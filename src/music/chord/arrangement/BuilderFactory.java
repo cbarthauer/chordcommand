428518d955c54220a4dba3a87b31a8cc37dcdda9
cbarthauer <devnull@localhost>
2012-06-30 09:52:08 -0400

    Remove BuilderFactory's dependency on ChordDefinitionStructure.

--- src/music/chord/arrangement/BuilderFactory.java
+++ src/music/chord/arrangement/BuilderFactory.java
@@ -30,26 +30,6 @@ public class BuilderFactory {
                 VoicePart.barbershopDefault()));
         
         return ninthBuilder;
-    }   
-    
-    public static VoicedChordBuilder getNinthBuilder(ChordDefinitionStructure struct) {
-        Voicing ninthVoicing = new VoicingImpl(VoicingValidator.NINTH_VALIDATOR);
-        ninthVoicing.addChordMember(ChordMember.ROOT);
-        ninthVoicing.addChordMember(ChordMember.SEVENTH);
-        ninthVoicing.addChordMember(ChordMember.NINTH);
-        ninthVoicing.addChordMember(ChordMember.THIRD);
-        
-        VoicedChordBuilder ninthBuilder = new VoicedChordBuilderImpl(
-            new VoicedChordConfig(
-                null,
-                null,
-                ninthVoicing,
-                defaultOctaveShift,
-                Duration.QUARTER,
-                VoicePart.barbershopDefault()),
-            struct);
-        
-        return ninthBuilder;
     }
 
     public static VoicedChordBuilder getSeventhBuilder(
@@ -74,26 +54,6 @@ public class BuilderFactory {
         return builder;
     }
 
-    public static VoicedChordBuilder getSeventhBuilder(ChordDefinitionStructure struct) {
-        Voicing seventhVoicing = new VoicingImpl(VoicingValidator.SEVENTH_VALIDATOR);
-        seventhVoicing.addChordMember(ChordMember.ROOT);
-        seventhVoicing.addChordMember(ChordMember.FIFTH);
-        seventhVoicing.addChordMember(ChordMember.SEVENTH);
-        seventhVoicing.addChordMember(ChordMember.THIRD);
-        
-        VoicedChordBuilder builder = new VoicedChordBuilderImpl(
-            new VoicedChordConfig(
-                null,
-                null,
-                seventhVoicing, 
-                defaultOctaveShift, 
-                Duration.QUARTER,
-                VoicePart.barbershopDefault()),
-            struct);
-        
-        return builder;
-    }
-
     public static VoicedChordBuilder getTriadBuilder(
             NoteName defaultRoot, 
             Quality defaultQuality) {
@@ -112,26 +72,6 @@ public class BuilderFactory {
                 defaultOctaveShift, 
                 Duration.QUARTER, 
                 VoicePart.barbershopDefault()));
-      
-        return triadBuilder;
-    }
-
-    public static VoicedChordBuilder getTriadBuilder(ChordDefinitionStructure struct) {
-        Voicing triadVoicing = new VoicingImpl(VoicingValidator.TRIAD_VALIDATOR);
-        triadVoicing.addChordMember(ChordMember.ROOT);
-        triadVoicing.addChordMember(ChordMember.FIFTH);
-        triadVoicing.addChordMember(ChordMember.ROOT);
-        triadVoicing.addChordMember(ChordMember.THIRD);
-        
-        VoicedChordBuilder triadBuilder = new VoicedChordBuilderImpl(
-            new VoicedChordConfig(
-                null,
-                null,
-                triadVoicing,
-                defaultOctaveShift, 
-                Duration.QUARTER, 
-                VoicePart.barbershopDefault()),
-            struct);
       
         return triadBuilder;
     }
