8d697d8b760bc57599361cea412b2b3cad66f170
cbarthauer <devnull@localhost>
2012-06-30 09:30:33 -0400

    Remove ChordVoicerImpl.

--- src/music/chord/arrangement/ChordVoicerFactory.java
+++ src/music/chord/arrangement/ChordVoicerFactory.java
@@ -1,23 +1,8 @@
 package music.chord.arrangement;
 
-import java.io.IOException;
-
 import music.chord.base.QualityRegistry;
 
-import org.antlr.runtime.RecognitionException;
-
 public final class ChordVoicerFactory {
-	public static ChordVoicer getInstance(ChordDefinitionStructure struct) 
-	        throws IOException, RecognitionException {
-		
-	    ChordVoicer voicer = new ChordVoicerImpl(
-		    new ClosestVoicingStrategy(new DerivedChordBuilder()),
-		    struct
-		);		
-		
-		return voicer;
-	}
-	
 	public static ChordVoicer getInstance(QualityRegistry qualities) {
         return new QualityChordVoicer(
                 new ClosestVoicingStrategy(new DerivedChordBuilder()),
