0a49cb015b3a3f7cde73868ce35d9c68b7385639
Chris Barthauer <cbarthauer@server>
2012-07-04 15:54:34 -0400

    Applied license header.

--- src/music/chord/base/QualityImpl.java
+++ src/music/chord/base/QualityImpl.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Chris Barthauer.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Chris Barthauer - initial API and implementation
+ ******************************************************************************/
 package music.chord.base;
 
 final class QualityImpl implements Quality {
